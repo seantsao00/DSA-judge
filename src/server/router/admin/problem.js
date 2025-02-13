cdb5550380e920a635c07fa89bd8d624d669e6c5
seantsao00 <seantsao00@gmail.com>
2025-02-14 00:19:00 +0800

    fix: improve code formatting and logging in problem router

--- src/server/router/admin/problem.js
+++ src/server/router/admin/problem.js
@@ -3,19 +3,18 @@ import _ from 'lodash';
 import wrap from 'express-async-wrap';
 import Problem from '/model/problem';
 import Submission from '/model/submission';
-import {requireAdmin} from '/utils';
 import tar from 'tar';
 import multer from 'multer';
 import path from 'path';
 import config from '/config';
 import fs from 'fs-extra';
-import {updateMeta} from './parseProblem';
+import { updateMeta } from './parseProblem';
 import logger from '/logger';
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/', limits: { fieldsize: 64 * 1024 }});
+const upload = multer({ dest: 'uploads/', limits: { fieldsize: 64 * 1024 } });
 
-function checkGzip (req, res, next) {
+function checkGzip(req, res, next) {
   const file = req.file;
   if (!file) {
     return res.status(404).send(`Please upload a file`);
@@ -23,22 +22,20 @@ function checkGzip (req, res, next) {
   next();
 }
 
-async function mkdir777 (dir) {
-  await fs.mkdir(dir);
-  await fs.chmod(dir, 0o777);
-}
-
-async function updateProblemByGzip (id, file) {
+async function updateProblemByGzip(id, file) {
   const destDir = path.join(config.dirs.problems, `${id}`);
   try {
     // remove the directory and create a new one
     await fs.remove(destDir);
     await fs.mkdir(destDir);
-    logger.info(`extract ${file} ${file.path} to ${destDir}`);
+    logger.info(`extract ${file.path} to ${destDir}`);
     // extract the archive to destination directory
-    await tar.extract({gzip: true, cwd: destDir, file: file.path, strip: 1});
+    await tar.extract({ gzip: true, cwd: destDir, file: file.path, strip: 1 });
+    logger.info(`extract ${file.path} to ${destDir} successfully`);
   } finally {
-    await fs.unlink(file.path, () => {});
+    fs.unlink(file.path, (err) => {
+      if (err) logger.error(`unlink ${file.path} failed with error ${err}`);
+    });
   }
 }
 
@@ -51,7 +48,7 @@ router.get('/', wrap(async (req, res) => {
 
 router.get('/:id', wrap(async (req, res) => {
   if (isNaN(req.params.id)) return res.status(400).send(`id must be a number`);
-  let problem = await Problem.findOne({_id: req.params.id});
+  let problem = await Problem.findOne({ _id: req.params.id });
   if (!problem) return res.status(404).send('Problem not found');
 
   problem = problem.toObject();
@@ -84,14 +81,16 @@ router.put('/',
       await updateProblemByGzip(id, file);
       await updateMeta(problem._id, problem);
       await problem.save();
+      logger.info(`Successfully create problem #${id}`);
     } catch (e) {
       logger.error(e);
       return res.status(500).send(e.toString());
     }
 
-    return res.send({id: problem._id});
+    return res.send({ id: problem._id });
   }
-  ));
+  )
+);
 
 router.put('/:id',
   upload.single('problem-file'),
@@ -116,7 +115,8 @@ router.put('/:id',
 
     return res.send(`Successfully update problem #${id}`);
   }
-  ));
+  )
+);
 
 router.post('/:id/updateTests',
   wrap(async (req, res) => {
@@ -149,7 +149,7 @@ router.put('/:id/settings', wrap(async (req, res) => {
     _.remove(upd, 'desc');
   }
 
-  const problem = await Problem.findOne({_id: req.params.id});
+  const problem = await Problem.findOne({ _id: req.params.id });
   if (!problem) return res.status(404).send('Problem not found');
   _.assignWith(problem, upd);
 
@@ -177,12 +177,12 @@ router.post('/:id/rejudge', wrap(async (req, res) => {
   if (isNaN(req.params.id)) return res.status(400).send(`id must be a number`);
   try {
     await Submission.update(
-      {problem: req.params.id},
+      { problem: req.params.id },
       {
-        $set: {'status': 'pending-rejudge', 'result': null},
-        $unset: {'runtime': ''}
+        $set: { 'status': 'pending-rejudge', 'result': null },
+        $unset: { 'runtime': '' }
       },
-      {multi: true}
+      { multi: true }
     );
   } catch (e) {
     logger.error(`router /${req.params.id}/rejudge failed with error ${e}`);
