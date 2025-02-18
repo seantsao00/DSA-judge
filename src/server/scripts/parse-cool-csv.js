1f69bad14d2e46822344a349c2d77ab667e6d380
seantsao00 <seantsao00@gmail.com>
2025-02-18 14:24:32 +0800

    feat: add CSV parsing script to transform data from NTU Cool form to the form other scripts accept

---
+++ src/server/scripts/parse-cool-csv.js
@@ -0,0 +1,51 @@
+import csvParser from 'csv-parser';
+import fs from 'fs';
+import { ArgumentParser } from 'argparse';
+import { createObjectCsvWriter } from 'csv-writer';
+
+const main = async () => {
+  const parser = new ArgumentParser({
+    description: 'Parse the Cool CSV file',
+    addHelp: true
+  });
+
+  parser.addArgument('source', { help: 'The Cool CSV file' });
+  parser.addArgument('destination', { help: 'The destination CSV file' });
+
+  const { source, destination } = parser.parseArgs();
+
+  const results = [];
+
+  fs.createReadStream(source)
+    .pipe(csvParser())
+    .on('data', (row) => {
+      if (row['身份'] !== '學生') throw new Error('Only students are allowed');
+
+      results.push({
+        ID: row['學號'],
+        name: row['姓名'],
+        email: row['信箱'],
+        role: 'student',
+      });
+    })
+    .on('end', () => {
+      const csvWriter = createObjectCsvWriter({
+        path: destination,
+        header: [
+          { id: 'ID', title: 'ID' },
+          { id: 'name', title: 'name' },
+          { id: 'email', title: 'email' },
+          { id: 'role', title: 'role' },
+        ],
+      });
+
+      csvWriter.writeRecords(results).then(() => {
+        console.log('CSV file successfully written to', destination);
+      });
+    })
+    .on('error', (err) => {
+      console.error('Error reading the CSV file:', err);
+    });
+};
+
+main();
